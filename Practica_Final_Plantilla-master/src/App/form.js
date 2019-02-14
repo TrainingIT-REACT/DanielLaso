02b88e1277e7a7db065d39a8f94ef1bc8ca912d2
Daniel <lavida93@gmail.com>
2019-02-14 21:14:12 +0100

    .

--- Practica_Final_Plantilla-master/src/App/form.js
+++ Practica_Final_Plantilla-master/src/App/form.js
@@ -3,20 +3,15 @@ import React from 'react';
 class Formulario extends React.Component {
     render() {
         return (
-
-
-            <div className="row">
-                <div className="four columns">
-                    <label htmlFor="name">Nombre</label>
-                    <input id="name" placeholder="Nombre Canción" type="text" />
+            <div>
+                <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
-               
+                <button type="submit" className="btn btn-primary">Submit</button>
             </div>
-        
-
         );
-        
-        
     }
 }
 export default Formulario;
